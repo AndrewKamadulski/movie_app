bebcf9e26870deb17c1b4b2e8598ed19d6bed49a
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-08-03 19:09:02 -0500

    hibernate working

--- server/src/main/java/com/andrewkamadulski/movie_app/config/MyDataRestConfig.java
+++ server/src/main/java/com/andrewkamadulski/movie_app/config/MyDataRestConfig.java
@@ -4,7 +4,6 @@ import com.andrewkamadulski.movie_app.entity.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
@@ -16,30 +15,15 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
                                                      CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {
-                HttpMethod.POST,
-                HttpMethod.PATCH,
-                HttpMethod.DELETE,
-                HttpMethod.PUT};
+
 
         config.exposeIdsFor(User.class);
 
-
-        disableHttpMethods(User.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
     }
 
-    private void disableHttpMethods(Class theClass,
-                                    RepositoryRestConfiguration config,
-                                    HttpMethod[] theUnsupportedActions) {
-        config.getExposureConfiguration()
-                .forDomainType(theClass)
-                .withItemExposure((metdata, httpMethods) ->
-                        httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) ->
-                        httpMethods.disable(theUnsupportedActions));
-    }
+
 }
