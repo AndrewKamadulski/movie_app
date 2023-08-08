90d96a82dc17d1c1cec800bdac91a90940a62f5e
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-08-08 15:24:36 -0500

    changed buttons to links in navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./navbar.css"
 
 export const Navbar = () => {
@@ -5,20 +6,20 @@ export const Navbar = () => {
 
 <nav className="navbar navbar-dark navbar-expand-lg">
   <div className="container-fluid">
-    <button className="navbar-brand fs-1 px-3 text-dark">Horror Scorer</button>    
+    <Link to="/" className="navbar-brand fs-1 px-3 text-light">Horror Scorer</Link>    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>      
     <div className="collapse navbar-collapse fs-4" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">    
         <li className="nav-item">
-          <button className="nav-link text-dark">Login</button>
+          <Link to="/" className="nav-link text-light">Login</Link>
         </li>      
         <li className="nav-item">
-          <button className="nav-link text-dark" >SignUp</button>
+          <Link to="/" className="nav-link text-light" >SignUp</Link>
         </li>
         <li className="nav-item">
-          <button className="nav-link text-dark" >UserName</button>
+          <Link to="/" className="nav-link text-light" >UserName</Link>
         </li>
       </ul> 
   
