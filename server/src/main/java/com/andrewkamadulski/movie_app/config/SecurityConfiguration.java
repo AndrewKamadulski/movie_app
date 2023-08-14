e9f01ddd6a8a61d6f2bb2791854943da52a93bc7
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-08-14 08:12:04 -0500

    security filter chain

--- server/src/main/java/com/andrewkamadulski/movie_app/config/SecurityConfiguration.java
+++ server/src/main/java/com/andrewkamadulski/movie_app/config/SecurityConfiguration.java
@@ -8,26 +8,64 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-
+//import com.okta.spring.boot.oauth.Okta;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.accept.ContentNegotiationStrategy;
+//import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        // Disable Cross Site Request Forgery
+//        http.csrf().disable();
+//
+//        // Protect endpoints at /api/<type>/secure
+//        http.authorizeHttpRequests(requests ->
+//                        requests
+//                                .requestMatchers("/*"
+//                                )
+//                                .permitAll())
+//                .oauth2ResourceServer()
+//                .jwt();
+//
+//        // Add CORS filters
+//        http.cors();
+//
+//        // Add content negotiation strategy
+//        http.setSharedObject(ContentNegotiationStrategy.class,
+//                new HeaderContentNegotiationStrategy());
+//
+//        // Force a non-empty response body for 401's to make the response friendly
+//        Okta.configureResourceServer401ResponseBody(http);
+//
+//        return http.build();
+//    }
+//
+//}
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("api/reviews/search/**"
-                                ).permitAll().requestMatchers("api/replies"
-                        ).permitAll()
-                )
-                .httpBasic(withDefaults());
-
         // Disable Cross Site Request Forgery
         http.csrf().disable();
+
+        // Protect endpoints at /api/<type>/secure
+        http.authorizeRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/movies/secure/**")
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
 
         // Add CORS filters
         http.cors();
