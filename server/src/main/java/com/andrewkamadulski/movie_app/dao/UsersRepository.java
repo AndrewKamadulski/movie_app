40f247af2cba57017dbb10f1836a9ff3ee599aa7
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-08-29 15:59:28 -0500

    user data

--- server/src/main/java/com/andrewkamadulski/movie_app/dao/UsersRepository.java
+++ server/src/main/java/com/andrewkamadulski/movie_app/dao/UsersRepository.java
@@ -1,9 +1,17 @@
 package com.andrewkamadulski.movie_app.dao;
 
 import com.andrewkamadulski.movie_app.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface UsersRepository extends JpaRepository<User, Long> {
+
+    @Query(
+            value = "SELECT  * FROM users WHERE  name = :userName", nativeQuery = true)
+    Page<User> findByUserName(@Param("userName") String userName, Pageable pageable);
 
 }
